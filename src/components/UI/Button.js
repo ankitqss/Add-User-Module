49ad41396efc99df6264b17064aa9b711e36d1ce
ankitqss <ankitgupta@qsstechnosoft.com>
2023-04-06 18:32:06 +0530

    first commit

---
+++ src/components/UI/Button.js
@@ -0,0 +1,16 @@
+import React from "react";
+import classes from "./Button.module.css";
+
+const Button = (props) => {
+  return (
+    <Button
+      className={classes.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </Button>
+  );
+};
+
+export default Button;
